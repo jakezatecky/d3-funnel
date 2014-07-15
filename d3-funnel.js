abe618b1a36c39b0e622619086f02b75bd314a9e
Jake Zatecky <jake.zatecky@gmail.com>
2014-07-15 00:02:59 -0400

    Added some more detailed comments on the options parameter. Also added a very small piece of code to remove any previous drawings with D3Funnel::draw is invoked.

--- d3-funnel.js
+++ d3-funnel.js
@@ -11,6 +11,19 @@
 	 *
 	 * @param {array}  data    A list of rows containing a category and a count.
 	 * @param {Object} options An optional configuration object for chart options.
+	 *
+	 * @param {int}    options.width       Initial width. Specified in pixels.
+	 * @param {int}    options.height      Chart height. Specified in pixels.
+	 * @param {int}    options.bottomWidth Specifies the width, in pixels, that the bottom of the
+	 *                                     funnel chart should be. This will affect the overall
+	 *                                     appareance of the chart.
+	 * @param {int}    options.bottomPinch How many sections (from the bottom) should be "pinched"
+	 *                                     to have fixed width defined by options.bottomWidth.
+	 * @param {bool}   options.isCurved    Whether or not the funnel is curved.
+	 * @param {int}    options.curveHeight The height of the curves. Only functional if isCurved
+	 *                                     is set.
+	 * @param {string} options.fillType    The section background type. Either "solid" or "gradient".
+	 * @param {bool}   options.isPyramid   Whether or not the funnel should be inverted to a pyramid.
 	 */
 	function D3Funnel ( data, options )
 	{
@@ -25,6 +38,9 @@
 	 */
 	D3Funnel.prototype.draw = function ( selector )
 	{
+
+		// Remove any previous drawings
+		d3.select ( selector ).selectAll ( "svg" ).remove ();
 
 	};  // End draw
 
