f5d301c30e607f6f1e3cf8236d283788241c5a83
Jake Zatecky <jake.zatecky@gmail.com>
2015-06-10 21:56:11 -0400

    Switch from Grunt to Gulp

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp   = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs   = require('gulp-jscs');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var header = require('gulp-header');
+var pkg    = require('./package.json');
+
+var banner = '/*! <%= pkg.name %> - v<%= pkg.version %> | <%= new Date().getFullYear() %> */\n';
+
+gulp.task('build', function() {
+    return gulp.src('./src/d3-funnel/d3-funnel.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'))
+        .pipe(jscs({
+            verbose: true
+        }))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(rename({
+            extname: '.min.js'}
+        ))
+        .pipe(uglify())
+        .pipe(header(banner, {
+            pkg: pkg
+        }))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('default', ['build']);
