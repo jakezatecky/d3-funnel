c1304a755d2f2b0faa789f01a85ba8bae17ed5dc
Jake Zatecky <jake.zatecky@gmail.com>
2016-07-12 23:01:37 -0400

    Fix Gulp streams

--- gulpfile.js
+++ gulpfile.js
@@ -10,33 +10,33 @@ const pkg = require('./package.json');
 
 const banner = '/*! <%= pkg.name %> - v<%= pkg.version %> | <%= new Date().getFullYear() %> */\n';
 
-gulp.task('test-format', () => {
+gulp.task('test-format', () =>
 	gulp.src(['./src/d3-funnel/**/*.js'])
 		.pipe(eslint())
 		.pipe(eslint.format())
-		.pipe(eslint.failOnError());
-});
+		.pipe(eslint.failOnError())
+);
 
-gulp.task('compile-test', () => {
+gulp.task('compile-test', () =>
 	gulp.src(['./test/index.js'])
 		.pipe(webpack(webpackConfig))
-		.pipe(gulp.dest('./test/compiled/'));
-});
+		.pipe(gulp.dest('./test/compiled/'))
+);
 
-gulp.task('test-mocha', ['compile-test'], () => {
+gulp.task('test-mocha', ['compile-test'], () =>
 	gulp.src(['test/test.html'])
-		.pipe(mocha({ reporter: 'spec' }));
-});
+		.pipe(mocha({ reporter: 'spec' }))
+);
 
 gulp.task('test', ['test-format', 'test-mocha']);
 
-gulp.task('compile-build', () => {
+gulp.task('compile-build', () =>
 	gulp.src(['./src/index.js'])
 		.pipe(webpack(webpackConfig))
-		.pipe(gulp.dest('./compiled/'));
-});
+		.pipe(gulp.dest('./compiled/'))
+);
 
-gulp.task('build', ['test', 'compile-build'], () => {
+gulp.task('build', ['test', 'compile-build'], () =>
 	gulp.src(['./compiled/d3-funnel.js'])
 		.pipe(gulp.dest('./dist/'))
 		.pipe(rename({
@@ -44,11 +44,11 @@ gulp.task('build', ['test', 'compile-build'], () => {
 		}))
 		.pipe(uglify())
 		.pipe(header(banner, { pkg }))
-		.pipe(gulp.dest('./dist/'));
-});
+		.pipe(gulp.dest('./dist/'))
+);
 
-gulp.task('watch', () => {
-	gulp.watch(['./src/d3-funnel/**/*.js'], ['build']);
-});
+gulp.task('watch', () =>
+	gulp.watch(['./src/d3-funnel/**/*.js'], ['build'])
+);
 
 gulp.task('default', ['build']);
