beb97650945e4c809a495aeb2b03c8ae4420f204
Jake Zatecky <jake.zatecky@gmail.com>
2016-07-12 22:39:48 -0400

    Make gulpfile comply with ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,53 @@
-var gulp    = require('gulp');
-var eslint  = require('gulp-eslint');
-var mocha   = require('gulp-mocha-phantomjs');
-var rename  = require('gulp-rename');
-var uglify  = require('gulp-uglify');
-var header  = require('gulp-header');
-var webpack = require('webpack-stream');
-var pkg     = require('./package.json');
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha-phantomjs');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const header = require('gulp-header');
+const webpack = require('webpack-stream');
+const webpackConfig = require('./webpack.config.js');
+const pkg = require('./package.json');
 
-var banner = '/*! <%= pkg.name %> - v<%= pkg.version %> | <%= new Date().getFullYear() %> */\n';
+const banner = '/*! <%= pkg.name %> - v<%= pkg.version %> | <%= new Date().getFullYear() %> */\n';
 
-gulp.task('test-format', function () {
-	return gulp.src(['./src/d3-funnel/**/*.js'])
+gulp.task('test-format', () => {
+	gulp.src(['./src/d3-funnel/**/*.js'])
 		.pipe(eslint())
 		.pipe(eslint.format())
 		.pipe(eslint.failOnError());
 });
 
-gulp.task('compile-test', function () {
-	return gulp.src(['./test/index.js'])
-		.pipe(webpack(require('./webpack.config.js')))
+gulp.task('compile-test', () => {
+	gulp.src(['./test/index.js'])
+		.pipe(webpack(webpackConfig))
 		.pipe(gulp.dest('./test/compiled/'));
 });
 
-gulp.task('test-mocha', ['compile-test'], function () {
-	return gulp.src(['test/test.html'])
-		.pipe(mocha({reporter: 'spec'}));
+gulp.task('test-mocha', ['compile-test'], () => {
+	gulp.src(['test/test.html'])
+		.pipe(mocha({ reporter: 'spec' }));
 });
 
 gulp.task('test', ['test-format', 'test-mocha']);
 
-gulp.task('compile-build', function () {
-	return gulp.src(['./src/index.js'])
-		.pipe(webpack(require('./webpack.config.js')))
+gulp.task('compile-build', () => {
+	gulp.src(['./src/index.js'])
+		.pipe(webpack(webpackConfig))
 		.pipe(gulp.dest('./compiled/'));
 });
 
-gulp.task('build', ['test', 'compile-build'], function () {
-	return gulp.src(['./compiled/d3-funnel.js'])
+gulp.task('build', ['test', 'compile-build'], () => {
+	gulp.src(['./compiled/d3-funnel.js'])
 		.pipe(gulp.dest('./dist/'))
 		.pipe(rename({
 			extname: '.min.js',
 		}))
 		.pipe(uglify())
-		.pipe(header(banner, {
-			pkg: pkg,
-		}))
+		.pipe(header(banner, { pkg }))
 		.pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
 	gulp.watch(['./src/d3-funnel/**/*.js'], ['build']);
 });
 
