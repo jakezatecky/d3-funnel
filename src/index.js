c67fa4a6f5628d6775ea93e2f78e79f5914b728a
Jake Zatecky <jake.zatecky@gmail.com>
2023-04-04 11:11:27 -0400

    Publish v2.1.1 examples

--- src/index.js
+++
@@ -1,2 +0,0 @@
-// Export default to provide support for non-ES6 solutions
-module.exports = require('./d3-funnel/D3Funnel').default;
--- test/index.js
+++
@@ -1,4 +0,0 @@
-import './d3-funnel/Colorizer';
-import './d3-funnel/D3Funnel';
-import './d3-funnel/Navigator';
-import './d3-funnel/Utils';
