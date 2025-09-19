fbe5ff3e639d70224c57447486a8cc1a35e2375e
Jake Zatecky <jake.zatecky@gmail.com>
2025-09-18 23:59:12 -0400

    Remove unused linting globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
-import globals from 'globals';
 import takiyonConfig from 'eslint-config-takiyon';
+import globals from 'globals';
 
 import webpackConfig from './webpack.config.test.js';
 
@@ -17,9 +17,6 @@ export default [
         settings: {
             // Account for webpack.resolve.module imports
             'import/resolver': {
-                // Fixes Node resolution issues in ESLint v6
-                // https://github.com/benmosher/eslint-plugin-import/issues/1396
-                node: {},
                 webpack: {
                     config: webpackConfig,
                 },
@@ -31,13 +28,9 @@ export default [
         files: [
             'examples/**/*.js',
             'src/**/*.js',
-            'test/**/*.js',
         ],
         languageOptions: {
-            globals: {
-                APP_NAME: 'readonly',
-                ...globals.browser,
-            },
+            globals: globals.browser,
         },
     },
     {
@@ -45,7 +38,7 @@ export default [
         files: ['test/**/*.js'],
         languageOptions: {
             globals: {
-                APP_NAME: 'readonly',
+                ...globals.browser,
                 ...globals.mocha,
             },
         },
@@ -54,9 +47,7 @@ export default [
         // Build files
         files: ['*.js'],
         languageOptions: {
-            globals: {
-                ...globals.node,
-            },
+            globals: globals.node,
         },
     },
 ];
