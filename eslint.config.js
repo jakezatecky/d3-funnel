5172bed36c9baa33cad31cc64bcc9741cdded39d
Jake Zatecky <jake.zatecky@gmail.com>
2026-09-22 12:13:38 -0400

    Simplify ESLint + Webpack resolution

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,26 @@
 import { defineConfig } from 'eslint/config';
 import takiyonConfig from 'eslint-config-takiyon';
+import { createNodeResolver } from 'eslint-plugin-import-x';
 import globals from 'globals';
 
 import webpackConfig from './webpack.config.test.js';
-
-// Resolve issue with HTML Webpack Bundler causing circular references
-// https://github.com/webdiscus/html-bundler-webpack-plugin/issues/186
-delete webpackConfig.plugins;
 
 export default defineConfig([
     takiyonConfig,
     {
         files: ['**/*.{js,jsx}'],
         settings: {
-            // Account for webpack.resolve.module imports
-            'import-x/resolver': {
-                webpack: {
-                    config: webpackConfig,
-                },
-            },
+            // Account for webpack.resolve.alias imports
+            'import-x/resolver-next': [
+                createNodeResolver({
+                    alias: Object.fromEntries(
+                        Object.entries(webpackConfig.resolve.alias).map(([name, target]) => [
+                            name,
+                            [target],
+                        ]),
+                    ),
+                }),
+            ],
         },
     },
     {
@@ -49,27 +51,6 @@ export default defineConfig([
         ],
         languageOptions: {
             globals: globals.node,
-        },
-    },
-    {
-        // Development files
-        files: [
-            'test/**/*.js',
-            '*.js',
-        ],
-        settings: {
-            // Resolve `exports` fields for development files
-            'import-x/resolver': {
-                webpack: {
-                    config: {
-                        ...webpackConfig,
-                        resolve: {
-                            ...webpackConfig.resolve,
-                            conditionNames: ['node', 'import', 'require', 'default'],
-                        },
-                    },
-                },
-            },
         },
     },
 ]);
