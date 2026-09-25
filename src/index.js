fed32f9c64a028f561a84e5dc91f96d7c2e96e30
Jake Zatecky <jake.zatecky@gmail.com>
2026-09-25 14:08:26 -0400

    Publish v2.3.0 examples

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import D3Funnel from '#js/D3Funnel.js';
-
-export default D3Funnel;
--- test/index.js
+++
@@ -1,7 +0,0 @@
-// Because JSDom does not support SVGs properly this must run in a browser
-// https://github.com/jsdom/jsdom/issues/918
-
-import './d3-funnel/Colorizer.js';
-import './d3-funnel/D3Funnel.js';
-import './d3-funnel/Navigator.js';
-import './d3-funnel/Utils.js';
